9ebc9e47b8d14384515739a458a0cdda9c213754
shadawwuegidua <hdeyeheiw1106@163.com>
2026-01-27 15:35:49 +0800

    62 in python for dynamic programming

--- Dynamic Programming/62.py
+++ Dynamic Programming/62.py
@@ -38,4 +38,13 @@
 题目数据保证答案小于等于 2 * 109"""
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0]*n for i in range(m)]
+        dp = [[0]*n for _ in range(m)]
+        for i  in range(n):
+            dp[0][i]=1
+        for j in range(m):
+            dp[j][0]=1
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[i][j] = dp[i-1][j]+dp[i][j-1]
+        return dp[m-1][n-1]
+        
