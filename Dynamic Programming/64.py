f54acc0607adbef4c6e47447a7d306464a80859f
shadawwuegidua <hdeyeheiw1106@163.com>
2026-01-27 16:26:27 +0800

    a faster way for problem 64 in python by using 1 dimension list

--- Dynamic Programming/64.py
+++ Dynamic Programming/64.py
@@ -25,3 +25,41 @@ m == grid.length
 n == grid[i].length
 1 <= m, n <= 200
 0 <= grid[i][j] <= 200"""
+
+class Solution:
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m=len(grid)
+        n=len(grid[0])
+        dp = [[0]*n for _ in range(m)]
+        dp[0][0]=grid[0][0]
+        # for i in range(1,m):
+        #     dp[i][0]=dp[i-1][0]+grid[i][0]
+        for j in range(1,n):
+            dp[0][j]=dp[0][j-1]+grid[0][j]
+        for i in range(1,m):
+            dp[i][0]=dp[i-1][0]+grid[i][0]
+            for j in range(1,n):
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j]
+        return dp[m-1][n-1]
+    
+    def faster_minPathSum(self, grid: List[List[int]]) -> int:
+
+        m,n = len(grid), len(grid[0])
+
+        dp = [0]*n
+        dp[0] = grid[0][0]
+        for i in range(1,n):
+            dp[i] = dp[i-1]+grid[0][i]
+        # 这一步对于第一行，让dp这个一维数组记录第一行从左到右的累加和
+        for i in range(1,m):
+            for j in range(0,n):
+                if j == 0:
+                    dp[j] = dp[j] + grid[i][0]
+                    # dp[0]记录了第一列从上到下的累加和
+                    # 每次i变动，换行的时候 dp[0]都在记录当前grid[i][0]的值？
+                else:
+                    dp[j] = min(dp[j-1],dp[j])+grid[i][j]
+                    # 对于当前行来说，这一行执行时dp[j]还没有变动，记录的是上一次i即grid[i-1][j]的值
+                    # 所以dp[j-1]是上一次j变化，即同样的行，左边那一列所对应的grid[i][j-1]的值
+                    # 这样就完成了对于当前grid[i][j]的最小路径和的计算
+        return dp[n-1]
