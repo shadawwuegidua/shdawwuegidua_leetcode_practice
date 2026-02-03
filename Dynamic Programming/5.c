59311fdec768e946c71f067496d8466ed4d087db
shadawwuegidua <hdeyeheiw1106@163.com>
2026-02-04 00:39:05 +0800

    5 in python & C without dynamic programming

---
+++ Dynamic Programming/5.c
@@ -0,0 +1,58 @@
+/*5. 最长回文子串
+中等
+提示
+给你一个字符串 s，找到 s 中最长的 回文 子串。
+
+ 
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母组成
+ 
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+bool isPalindrome(char* s, int left, int right) {
+    while (left < right) {
+        if (s[left] != s[right]) {
+            return false;
+        }
+        left++;
+        right--;
+    }
+    return true;
+}
+char* longestPalindrome(char* s) {
+    int n = strlen(s);
+    if (n == 0) {
+        return "";
+    }
+    int maxLength = 1;
+    int start = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            if (isPalindrome(s, i, j)) {
+                int currentLength = j - i + 1;
+                if (currentLength > maxLength) {
+                    maxLength = currentLength;
+                    start = i;
+                }
+            }
+        }
+    }
+    s[start + maxLength] = '\0'; // 截断字符串
+    return &s[start];
+}
---
+++ Dynamic Programming/5.py
@@ -0,0 +1,50 @@
+"""5. 最长回文子串
+中等
+提示
+给你一个字符串 s，找到 s 中最长的 回文 子串。
+
+ 
+
+示例 1：
+
+输入：s = "babad"
+输出："bab"
+解释："aba" 同样是符合题意的答案。
+示例 2：
+
+输入：s = "cbbd"
+输出："bb"
+ 
+
+提示：
+
+1 <= s.length <= 1000
+s 仅由数字和英文字母组成
+ 
+"""
+
+class Solution:
+    def isPalindrome(self, s:str, left:int, right:int) -> bool:
+        while left<right:
+            if s[left]!=s[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        if n == 0:
+            return ""
+        # dp = [[False]*n for _ in range(n)]
+        max_len =1
+        start = 0
+        for i in range(n):
+            for j in range(i,n):
+                if(isPalindrome(s,i,j)):
+                    # dp[i][j]=True
+                    current_len = j - i + 1
+                    if current_len>max_len:
+                        max_len = j-i+1
+                        start = i
+        s[start+max_len]='\0'
+        return s[start:start+max_len]
